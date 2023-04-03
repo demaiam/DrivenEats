c44e8cd0c6f7ef1dac99f92bd30ef84e1ce73428
André <demaiam@gmail.com>
2023-04-03 14:34:12 -0300

    teste hub 2

--- script.js
+++ script.js
@@ -15,12 +15,11 @@ function fecharPedido() {
 
 function verificaPedido() {
     const botaoRodape = document.querySelector('.botao-rodape');
-
     const botaoRodapePronto = document.querySelector('.botao-rodape-pronto');
 
     if (nomePrato != '' && nomeBebida != '' && nomeSobremesa != '') {
-        botaoRodape.classList.add('escondido');
-        botaoRodapePronto.classList.remove('escondido');
+        botaoRodape.classList.toggle('escondido');
+        botaoRodapePronto.classList.toggle('escondido');
     }
 }
 
